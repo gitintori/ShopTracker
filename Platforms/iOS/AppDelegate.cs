997fad14f6a2628ee4b85f8a3ce352f6cf919dc4
GIOVANNA TINTORI <giovannatintori7@gmail.com>
2025-04-15 15:47:32 -0300

    Adicionar arquivos de projeto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShopTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
